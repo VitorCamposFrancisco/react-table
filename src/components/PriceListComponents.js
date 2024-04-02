89855d8bc51365ec368dddae3cc473e205816012
VitorCamposFrancisco <vitorcfrancisco@hotmail.com>
2024-04-01 22:17:25 -0300

    Aula 5 - Falta o map

--- src/components/PriceListComponents.js
+++ src/components/PriceListComponents.js
@@ -1,46 +1,38 @@
 import '../style/custom.css';
-import { useState } from 'react';
 import { Button, Col, Row, Table } from 'react-bootstrap';
 
 function PriceList(){
-    let id_produto  = 1;
-    let id_produto2 = 2;
-    let id_produto3 = 3;
 
-    let camisa1 = "Camisa Torcedor Palmeiras - P";
-    let camisa2 = "Camisa Jogador Palmeiras - M";
-    let camisa3 = "Camisa Torcedor Palmeiras - G";
+    let produto = {
+        id: 1,
+        produto: "Camisa Torcedor Palmeiras - P",
+        categoria: "Feminino",
+        quantidade: 1,
+        valorUnitario: 299.90,
+    };
+    let produto2 = {
+        id: 2,
+        produto: "Camisa Jogador Palmeiras - M",
+        categoria: "Masculino",
+        quantidade: 2,
+        valorUnitario: 499.90,
+    };
+    let produto3 = {
+        id: 3,
+        produto: "Camisa Torcedor Palmeiras - G",
+        categoria: "Masculino",
+        quantidade: 3,
+        valorUnitario: 299.90,
+    };
 
-    let categ1 = "Feminino";
-    let categ2 = "Masculino";
-    let categ3 = "Masculino";
-
-    let qtde1 = 1;
-    let qtde2 = 2;
-    let qtde3 = 3;
-    let qtdeTotal = qtde1 + qtde2 + qtde3;
-
-    let valorUnit1 = 299.90;
-    let valorUnit2 = 499.90;
-    let valorUnit3 = 299.90;
-    let valorUnitTotal = valorUnit1 + valorUnit2 + valorUnit3;
-
-    let valorTotal1 = valorUnit1
-    * qtde1;
-    let valorTotal2 = valorUnit2 * qtde2;
-    let valorTotal3 = valorUnit3 * qtde3;
-    let total = valorTotal1 + valorTotal2 + valorTotal3;
+    let lista_produto = {
+        produto,
+        produto2,
+        produto3,
+    }
 
 
-    const formatado  = valorUnitTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const formatado2 = valorTotal3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const formatado3 = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const Unit1      = valorUnit1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const Unit2      = valorUnit2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const Unit3      = valorUnit3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const total1     = valorTotal1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    const total2     = valorTotal2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-
+    
 
     return(
         <div>
@@ -75,74 +67,54 @@ function PriceList(){
         <tbody>
         <tr class="maisclaro">
             <td class="borda">
-                {id_produto}
+                {produto2.id}
             </td>
             <td class="borda">
-                {camisa1}
+                {produto2.produto}
             </td>
             <td class="borda">
-                {categ1}
+                {produto2.categoria}
             </td>
             <td class="borda">
-                {qtde1}
+                {produto2.quantidade}
             </td>
             <td class="borda">
-                {Unit1}
+                {produto2.valorUnitario}
             </td>
             <td class="borda">
-                {total1}
-            </td>   
-        </tr>
-        <tr class="maisclaro">
-            <td class="borda">
-                {id_produto2}
-            </td>
-            <td class="borda">
-                {camisa2}
-            </td>
-            <td class="borda">
-                {categ2}
-            </td>
-            <td class="borda">
-                {qtde2}
-            </td>
-            <td class="borda">
-                {Unit2}
-            </td>
-            <td class="borda">
-                {total2}
+                {produto2.quantidade * produto2.valorUnitario}
             </td>   
         </tr>
         <tr class="maisclaro"  >
             <td class="borda">
-                {id_produto3}
+                {produto3.id}
             </td>
             <td class="borda">
-                {camisa3}
+                {produto3.produto}
             </td>
             <td class="borda">
-                {categ3}
+                {produto3.categoria}
             </td>
             <td class="borda">
-                {qtde3}
+                {produto3.quantidade}
             </td>
             <td class="borda">
-                {Unit3}
+                {produto3.valorUnitario}
             </td>
             <td class="borda">
-                {formatado2}
+                {produto3.quantidade * produto3.valorUnitario}
             </td>   
         </tr>
         <tr class="escuro">
             <td colspan="3"></td>
             <td class="borda">
-                {qtdeTotal}
+                {produto.quantidade + produto2.quantidade + produto3.quantidade}
             </td>
             <td class="borda">
-                {formatado}
+                {produto.valorUnitario + produto2.valorUnitario + produto3.valorUnitario}
             </td>
             <td class="borda">
-                {formatado3}
+                {(produto.quantidade * produto.valorUnitario) + (produto2.quantidade * produto2.valorUnitario) + (produto3.quantidade * produto3.valorUnitario)}
             </td>
         </tr>
         </tbody>
